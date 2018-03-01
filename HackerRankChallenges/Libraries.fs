202b04d6e1f9fd2f2c91e6295814970c9d299ece
Jack Matusiewicz <Jack.Matusiewicz@outlook.com>
2018-03-01 23:54:29 +0000

    Moving separate cases into nested functions to aid readability

--- HackerRankChallenges/Libraries.fs
+++ HackerRankChallenges/Libraries.fs
@@ -15,34 +15,41 @@ module Libraries =
         let store = GroupStore ()
         let cityToGroup = Dictionary<int, Group> ()
 
+        let addTo (v : int) (g : Group) =
+            g.Add(v) |> ignore
+            cityToGroup.[v] <- g
+
+        let mergeGroups (larger : Group) (smaller : Group) =
+            for v in smaller do
+                larger.Add(v) |> ignore
+                cityToGroup.[v] <- larger
+
+        let makeNewGroup (a : int) (b : int) =
+            let ctr = store.Count
+            let newSet = HashSet<int>()
+            newSet.Add(a) |> ignore
+            newSet.Add(b) |> ignore
+            cityToGroup.[a] <- newSet
+            cityToGroup.[b] <- newSet
+            store.[ctr] <- newSet
+
         let addEdge ((a,b) : Edge) =
             let aHasSet = cityToGroup.ContainsKey a
             let bHasSet = cityToGroup.ContainsKey b
 
             match aHasSet,bHasSet with
             | false,false ->
-                let ctr = store.Count
-                let newSet = HashSet<int>()
-                newSet.Add(a) |> ignore
-                newSet.Add(b) |> ignore
-                cityToGroup.[a] <- newSet
-                cityToGroup.[b] <- newSet
-                store.[ctr] <- newSet
+                makeNewGroup a b
             | true, false ->
-                let aSet = cityToGroup.[a]
-                aSet.Add(b) |> ignore
-                cityToGroup.[b] <- aSet
+                addTo b (cityToGroup.[a])
             | false, true ->
-                let bSet = cityToGroup.[b]
-                bSet.Add(a) |> ignore
-                cityToGroup.[a] <- bSet
+                addTo a (cityToGroup.[b])
             | true, true ->
                 let aSet = cityToGroup.[a]
                 let bSet = cityToGroup.[b]
-
-                for b in bSet do
-                    aSet.Add(b) |> ignore
-                    cityToGroup.[b] <- aSet
+                if aSet.Count > bSet.Count then
+                    mergeGroups aSet bSet
+                else mergeGroups bSet aSet
 
         edges |> List.iter addEdge
 
